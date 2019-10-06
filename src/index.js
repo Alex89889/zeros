3523b6e3abd22ccc8f6201693b0cbdfc279f7b85
Alex89889 <mihal4encko.sasha@yandex.ru>
2019-10-06 17:32:37 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function zeros(expression) {
-  // your solution
+module.exports = function zeros(n) {
+	var arrNumber=[];
+	var colFact;
+	var result = 0;
+	var five = 0;
+	let evenNumber = false
+
+	function countZero (number){
+		var result = 0;
+    	while (number = Math.floor(number / 5)) result += number;
+		return result;
+	}
+	
+	var arr=n.split('*');
+	for (var i = 0; i < arr.length; i++) {
+		colFact=arr[i].split("!").length - 1;
+		arrNumber[i]=arr[i].split("!");
+		var	number = Number(arrNumber[i][0]);
+		if(colFact=='2'){
+			if (number % 2) {
+				five += countZero(number) - countZero((number - 1) / 2);	
+			}
+			else {
+				result += countZero(number/2);
+				evenNumber = true;
+			}
+		} 
+		else if(colFact=='1'){
+			result += countZero(number);
+		}
+		
+	}
+	
+	if(result || evenNumber){
+		result = result + five;
+	}
+
+	return result;
 }
